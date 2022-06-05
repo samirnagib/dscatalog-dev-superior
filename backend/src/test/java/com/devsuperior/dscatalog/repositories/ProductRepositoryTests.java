cbb75593bf33a072ff46e21dce1632bef37970df
Samir Nagib <samir.nagib@gmail.com>
2022-06-05 10:24:19 -0300

    02.Testes Automatizados: 02-20

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -19,16 +20,23 @@ public class ProductRepositoryTests {
 	
 	private long exintingId;
 	private long nonexintingId;
+	private long countTotalProducts;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		exintingId = 1L;
 		nonexintingId = 1000L;
+		countTotalProducts = 25L;
 	}
 	
 	@Test
 	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
 		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 	}
 	
 	
